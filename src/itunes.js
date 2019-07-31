3f65012dd4148a0d11b325d4a7ab52952a6a45fc
MathisBullinger <mathis.bullinger@code.berlin>
2019-07-31 18:08:28 +0200

    [parser] (feat): itunes helper functions

---
+++ src/itunes.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+
+export async function searchItunes({ term, limit }) {
+  console.log('\n\nsearch itunes\n\n')
+  if (!term) return []
+  return (await axios.get('https://itunes.apple.com/search', {
+    params: {
+      media: 'podcast',
+      term,
+      ...(limit && { limit }),
+    },
+  })).data.results
+}
+
+export async function getFeedUrl({ name, itunesId }) {
+  const results = await searchItunes({
+    term: itunesId || name,
+    ...(itunesId && { limit: 1 }),
+  })
+
+  if (itunesId) {
+    if (
+      results.length === 0 ||
+      results[0].collectionId.toString() !== itunesId.toString() ||
+      !results[0].feedUrl
+    )
+      return null
+    return results[0].feedUrl
+  } else return results.map(result => result.feedUrl).filter(feed => feed)
+}
+
+export function mapItunesResult(data) {
+  const artworks = Object.keys(data)
+    .filter(key => key.startsWith('artworkUrl'))
+    .map(key => ({
+      url: data[key],
+      size: parseInt(key.replace('artworkUrl', ''), 10) || null,
+    }))
+  return {
+    itunesId: data.collectionId,
+    name: data.collectionName,
+    creator: data.artistName,
+    ...(artworks && { artworks }),
+    feed: data.feedUrl,
+  }
+}
