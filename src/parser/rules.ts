735d419a88d81473db947f622b85833965b11649
MathisBullinger <mathis.bullinger@code.berlin>
2019-09-17 21:33:35 +0200

    (ref): align rules

--- src/parser/rules.ts
+++ src/parser/rules.ts
@@ -1,27 +1,28 @@
 import {
   build,
   textRule,
-  nodeRuleCst,
+  nodeRuleCst as custRule,
   nodeRule,
   lastEpisode as et,
   tags as n,
 } from './ruleUtils'
 
-const POD = 'CHANNEL'
-const EP = 'ITEM'
+const P = 'CHANNEL'
+const E = 'ITEM'
 
+// prettier-ignore
 export default build({
-  'pod.title': textRule(POD, n.title, 'name'),
-  'pod.link': textRule(POD, n.link, 'link'),
-  'pod.lang': textRule(POD, n.lang, 'lang'),
-  'pod.it:author': textRule(POD, n.itunes.author, 'creator'),
-  'pod.description': textRule(POD, n.description, 'description', 1),
-  'pod.it:summary': textRule(POD, n.itunes.summary, 'description', 2),
-  'pod.it:subtitle': textRule(POD, n.itunes.subtitle, 'subtitle'),
+  'pod.title':        textRule(P, n.title,            'name'),
+  'pod.link':         textRule(P, n.link,             'link'),
+  'pod.lang':         textRule(P, n.lang,             'lang'),
+  'pod.it:author':    textRule(P, n.itunes.author,    'creator'),
+  'pod.description':  textRule(P, n.description,      'description', 1),
+  'pod.it:summary':   textRule(P, n.itunes.summary,   'description', 2),
+  'pod.it:subtitle':  textRule(P, n.itunes.subtitle,  'subtitle'),
 
-  'pod.item': nodeRuleCst(POD, n.item, pod => pod.episodes.push({})),
-  'ep.title': textRule(EP, n.title, et('title')),
-  'ep.date': textRule(EP, n.pubdate, et('date')),
-  'ep.enclosure': nodeRule(EP, n.enclosure, et('file'), ({ URL }) => URL, 1),
-  'ep.m:con': nodeRule(EP, n.media.content, et('file'), ({ URL }) => URL, 2),
+  'pod.item':         custRule(P, n.item,             pod => pod.episodes.push({})),
+  'ep.title':         textRule(E, n.title,            et('title')),
+  'ep.date':          textRule(E, n.pubdate,          et('date')),
+  'ep.enclosure':     nodeRule(E, n.enclosure,        et('file'), ({ URL }) => URL, 1),
+  'ep.m:con':         nodeRule(E, n.media.content,    et('file'), ({ URL }) => URL, 2),
 })
