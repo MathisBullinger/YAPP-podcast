af18621cdb3d0f9e92f8531ab965da7d193e76cd
MathisBullinger <mathis.bullinger@code.berlin>
2019-09-19 17:24:52 +0200

    (fix): include typescript declarations

---
+++ src/podcast.d.ts
@@ -0,0 +1,22 @@
+interface Podcast extends Meta {
+  episodes: Episode[]
+}
+
+interface Meta {
+  creator?: string
+  name?: string
+  link?: string
+  language?: string
+  description?: string
+  subtitle?: string
+  feed?: string
+}
+
+interface Episode {
+  title?: string
+  description?: string
+  date?: number
+  file?: string
+  duration?: number
+  link?: string
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const slsw = require('serverless-webpack')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
   devtool: 'source-map',
-  mode: 'development',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  externals: [nodeExternals()],
   optimization: {
     minimize: false,
   },
