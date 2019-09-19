e71167541ad0bb4bcbd4f41a0538686d94255669
MathisBullinger <mathis.bullinger@code.berlin>
2019-09-19 20:36:39 +0200

    (fix): remove webpack externals & babel regenerator runtime

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
 const slsw = require('serverless-webpack')
-const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
   devtool: 'source-map',
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-  externals: [nodeExternals()],
   optimization: {
     minimize: false,
   },
